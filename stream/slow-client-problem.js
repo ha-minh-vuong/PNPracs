43b4d2ba143e9f5aceafef97caf491388081efd5
Ha Minh Vuong <vuong.haminh@gmail.com>
2013-08-06 21:17:11 +0700

    slow client problem ex.

---
+++ stream/slow-client-problem.js
@@ -0,0 +1,31 @@
+/**
+ * slow-client-problem.js
+ * PNPracs
+ *
+ * Created by Vuong on 8/6/13.
+ * Copyright (c) 2013 Vuong. All rights reserved.
+ *
+ */
+
+
+var fs = require('fs');
+
+require('http').createServer(function(req, res){
+
+  var rs = fs.createReadStream('/path/to/file');
+
+  rs.on('data', function(data){
+    if (!res.write(data)){
+      rs.pause();   // pause the readable stream if the write command can not flush it to the kernel.
+    }
+  });
+
+  res.on('drain', function(){
+    rs.resume();   // resume the readable stream when the writable stream is drained.
+  });
+
+  rs.on('end', function(){
+    res.end();
+  });
+
+}).listen(8080);
