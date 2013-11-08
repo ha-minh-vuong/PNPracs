99f7fd7d8de9196908c2a9eb50c1adfb1246e695
Ha Minh Vuong <vuong.haminh@gmail.com>
2013-11-08 14:29:25 +0700

    http response ex.

---
+++ http/http-response.js
@@ -0,0 +1,8 @@
+
+require('http').createServer(function (req, res) {
+	res.writeHead(200, {
+		'Content-Type': 'text/plain', 
+		'Cache-Control': 'max-age=3600'
+		})
+	res.end('Hello world');
+}).listen(55555);
